c5daf3e7eb20db5f2b1651ad1ca3e132c89a9957
guyuqiang <guyuqiang@huishoubao.com.cn>
2020-09-08 16:15:34 +0800

    优化选择排序

--- algorithm/SelectSort.java
+++ algorithm/SelectSort.java
@@ -3,6 +3,7 @@ import java.util.Comparator;
 
 /**
  * javac -cp .:stdlib.jar SelectSort.java
+ * java -cp .:stdlib.jar SelectSort
  */
 public class SelectSort{
 
@@ -14,15 +15,20 @@ public class SelectSort{
         int length = array.length;
         for (int i = 0; i < length; i++) {
             int minIndex = i;
-            for (int j = i; j < length; j++) {
+            for (int j = i+1; j < length; j++) {
                 if(less(array[j],array[minIndex])){
-                    exchange(array,j,minIndex);
+                    minIndex = j;//不需要即时交换，只需找出最小值的index即可，内循环后再交换值
                 }
             }
+            //一次外循环只交换一次
+            exchange(array,i,minIndex);
         }
     }
 
     private static void exchange(Comparable[] list,int index1,int index2){
+        if (index1 == index2) {
+            return;
+        }
         int length = list.length;
         if(index1<0 || index2<0 || index1>=length || index2>=length){
             throw new IndexOutOfBoundsException();
@@ -44,8 +50,14 @@ public class SelectSort{
     }
 
     public static void main(String[] args){
-        String[] test = {"b","a","d","c","e","f"};
-        SelectSort.sort(test);
+        String[] test = {"b","d","a","c","e","f"};
+        sort(test);
         printArray(test);
+        String[] test1 = {};
+        sort(test1);
+        printArray(test1);
+        String[] test2 = {"d","1","c","3","a","2"};
+        sort(test2);
+        printArray(test2);
     }
 }
